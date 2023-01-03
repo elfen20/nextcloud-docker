63c7eb51bd7cd0504f6658d20f4a09fa2e413bc1
Elfen Zwanzig <44099879+elfen20@users.noreply.github.com>
2023-01-03 08:53:59 +0100

    use the environment variable for journaling mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # entrypoint.sh
-echo "Resetting DB Journal Mode..."
-dbfile=( /nextcloud/.*.db )
-/usr/bin/sqlite3 $dbfile 'PRAGMA journal_mode = delete;'
-
 echo "Running nextcloud sync.."
 nextcloudcmd --unsyncedfolders /config/unsyncedfolders /nextcloud https://$NEXTCLOUD_USER:$NEXTCLOUD_PWD@$NEXTCLOUD_SRV
